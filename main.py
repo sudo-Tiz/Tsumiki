81d0fa01370067798d2cb1e1c190fa32d171486b
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-11-27 22:05:48 +0545

    feat: add css monitor

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from fabric import Application
-from fabric.utils import get_relative_path
+from fabric.utils import get_relative_path, monitor_file
+
+from loguru import logger
 
 from bar import StatusBar
+
+
+def apply_style(app: Application):
+    logger.info("[Main] CSS applied")
+    app.set_stylesheet_from_file(get_relative_path("styles/main.css"))
+
 
 if __name__ == "__main__":
     # Create the status bar
     bar = StatusBar()
     # Initialize the application with the status bar
     app = Application("bar", bar)
-    # Set the stylesheet for the application
-    app.set_stylesheet_from_file(get_relative_path("styles/main.css"))
+
+    # Monitor main.css file for changes
+    main_css_file = monitor_file(get_relative_path("styles/main.css"))
+    main_css_file.connect("changed", lambda *args: apply_style(app))
+
+    # Monitor colors.css file for changes
+    color_css_file = monitor_file(get_relative_path("styles/theme.css"))
+    color_css_file.connect("changed", lambda *args: apply_style(app))
+
+    apply_style(app)
 
     # Run the application
     app.run()
