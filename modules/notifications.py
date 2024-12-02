b45bd6ff8e3e82f5301ea54b9ee348ddbe85a125
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-12-02 15:21:47 +0545

    fix: notification close position

--- modules/notifications.py
+++ modules/notifications.py
@@ -77,6 +77,7 @@ class NotificationWidget(Box):
 
         header_container.pack_end(
             Box(
+                v_align="start",
                 children=(
                     Button(
                         image=Image(
