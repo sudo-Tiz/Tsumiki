0f685285d9e2baa62bd3d2292360242429715dbe
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-12-30 21:36:56 +0545

    fix: automate virtual environment setup by installing dependencies in start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ cd $HOME/bar
 if [ ! -d .venv ]; then
   echo "venv does not exist. Creating venv..."
   python3 -m venv .venv
+  pip install -r requirements.txt
 fi
 source .venv/bin/activate
 python3 main.py
