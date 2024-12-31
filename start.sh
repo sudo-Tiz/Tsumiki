2d4402d8657b4783d9d909b5f09d9294c9041368
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-12-31 20:59:46 +0545

    feat: add init script

--- start.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-cd $HOME/bar
-if [ ! -d .venv ]; then
-  echo "venv does not exist. Creating venv..."
-  python3 -m venv .venv
-  pip install -r requirements.txt
-fi
-source .venv/bin/activate
-python3 main.py
