19496b5195d956bd6aff6d4d7ebafe1a08eb2407
🤖 automated <cron@job>
2025-02-05 01:06:27 +0545

    devina  laptop 💻 (4)

--- widgets/__init__.py
+++ widgets/__init__.py
@@ -11,6 +11,7 @@ from .keyboard_layout import *
 from .language import *
 from .microphone import *
 from .mpris import *
+from .overview import *
 from .power_button import *
 from .recorder import *
 from .stats import *
