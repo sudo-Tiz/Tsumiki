4a425132498f542854f3bc85503af96e840f343d
Rubin Bhandari <roobin.bhandari@gmail.com>
2025-02-23 09:42:27 +0545

    refactor: encapsulate configuration handling in HydeConfig class and improve CSS settings application

--- utils/config.py
+++ utils/config.py
@@ -2,36 +2,56 @@ import json
 import os
 
 from fabric.utils import get_relative_path
+from loguru import logger
 
 from .constants import DEFAULT_CONFIG
 from .functions import exclude_keys, merge_defaults, validate_widgets
 from .widget_settings import BarConfig
 
 
-# Function to read the configuration file
-def read_config():
-    config_file = get_relative_path("../config.json")
-    if not os.path.exists(config_file):
-        with open(config_file, "w") as destination_file:
-            json.dump(DEFAULT_CONFIG, destination_file, indent=4, ensure_ascii=False)
-        return DEFAULT_CONFIG
+class HydeConfig:
+    "A class to read the configuration file and return the default configuration"
 
-    with open(config_file) as file:
-        # Load JSON data into a Python dictionary
-        data = json.load(file)
-    return data
+    # Function to read the configuration file
+    def read_config(self) -> dict:
+        config_file = get_relative_path("../config.json")
+        if not os.path.exists(config_file):
+            with open(config_file, "w") as destination_file:
+                json.dump(
+                    DEFAULT_CONFIG, destination_file, indent=4, ensure_ascii=False
+                )
+            return DEFAULT_CONFIG
+
+        with open(config_file) as file:
+            # Load JSON data into a Python dictionary
+            data = json.load(file)
+        return data
+
+    def default_config(self) -> BarConfig:
+        # Read the configuration from the JSON file
+        parsed_data = self.read_config()
+
+        validate_widgets(parsed_data, DEFAULT_CONFIG)
+
+        for key in exclude_keys(DEFAULT_CONFIG, ["$schema"]):
+            parsed_data[key] = merge_defaults(
+                parsed_data.get(key, {}), DEFAULT_CONFIG[key]
+            )
+
+        return parsed_data
+
+    def set_css_settings(self):
+        logger.info("Applying css settings...")
+
+        settings = ""
+        # for setting in self.config["css_settings"]:
+        for setting in self.try_get_property("css_settings", [], True):
+            settings += (
+                f"${setting}: {self.try_get_property(setting, 'css_settings')};\n"
+            )
+
+        with open(get_relative_path("styles/_settings.scss"), "w") as f:
+            f.write(settings)
 
 
-def default_config() -> BarConfig:
-    # Read the configuration from the JSON file
-    parsed_data = read_config()
-
-    validate_widgets(parsed_data, DEFAULT_CONFIG)
-
-    for key in exclude_keys(DEFAULT_CONFIG, ["$schema"]):
-        parsed_data[key] = merge_defaults(parsed_data.get(key, {}), DEFAULT_CONFIG[key])
-
-    return parsed_data
-
-
-widget_config = default_config()
+widget_config = HydeConfig().default_config()
