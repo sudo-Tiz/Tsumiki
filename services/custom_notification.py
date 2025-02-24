c85d12682a85b6ed9edaf9e49ae8042b0448b5a3
Rubin Bhandari <roobin.bhandari@gmail.com>
2025-02-25 03:56:33 +0545

    refactor: streamline notification reading and writing methods for clarity

--- services/custom_notification.py
+++ services/custom_notification.py
@@ -46,70 +46,52 @@ class CustomNotifications(Notifications):
 
     def __init__(self, **kwargs):
         super().__init__()
-        self.all_notifications = self.do_read_notifications()
+        self.all_notifications = self._read_notifications()
         self._count = len(self.all_notifications)
-
         self.deserialized_notifications = []
         self._dont_disturb = False
 
-    def do_read_notifications(self):
-        # Check if the cache file exists and read existing data
+    def _read_notifications(self):
+        """Read notifications from the cache file."""
         if os.path.exists(NOTIFICATION_CACHE_FILE):
-            with open(NOTIFICATION_CACHE_FILE, "r") as file:
-                try:
-                    # Load existing data if the file is not empty
-                    existing_data = json.load(file)
-                except (json.JSONDecodeError, KeyError, ValueError, IndexError) as e:
-                    logger.error(f"{Colors.INFO}[Notification]", e)
-                    existing_data = []  # If the file is empty or malformed
-        else:
-            existing_data = []
-        return existing_data
+            try:
+                with open(NOTIFICATION_CACHE_FILE, "r") as file:
+                    return json.load(file)
+            except (json.JSONDecodeError, KeyError, ValueError, IndexError) as e:
+                logger.error(f"{Colors.INFO}[Notification] {e}")
+        return []
 
     def remove_notification(self, id: int):
         """Remove the notification of given id."""
         item = next((p for p in self.all_notifications if p["id"] == id), None)
-        index = self.all_notifications.index(item)
+        if item:
+            self.all_notifications.remove(item)
+            self._count -= 1
+            self._write_notifications(self.all_notifications)
+            self.emit("notification_count", self._count)
 
-        self.write_notifications(self.all_notifications)
-
-        self.all_notifications.pop(index)
-        self._count -= 1
-        self.emit("notification_count", self._count)
-
-        # Emit clear_all signal if there are no notifications left
-        if self._count == 0:
-            self.emit("clear_all", True)
+            # Emit clear_all signal if there are no notifications left
+            if self._count == 0:
+                self.emit("clear_all", True)
 
     def cache_notification(self, data: Notification):
         """Cache the notification."""
-
-        existing_data = self.all_notifications
-
         serialized_data = data.serialize()
         serialized_data.update({"id": self._count + 1})
-
-        # Append the new notification to the existing data
-        existing_data.append(serialized_data)
-
-        # Write the updated data back to the cache file
-        self.write_notifications(existing_data)
-
+        self.all_notifications.append(serialized_data)
         self._count += 1
-        self.all_notifications = existing_data
+        self._write_notifications(self.all_notifications)
         self.emit("notification_count", self._count)
 
     def clear_all_notifications(self):
         """Empty the notifications."""
-        self._notifications = []
+        self.all_notifications = []
         self._count = 0
-
-        # Write the updated data back to the cache file
-        self.write_notifications(self._notifications)
+        self._write_notifications(self.all_notifications)
         self.emit("clear_all", True)
         self.emit("notification_count", self._count)
 
-    def write_notifications(self, data):
+    def _write_notifications(self, data):
         """Write the notifications to the cache file."""
         with open(NOTIFICATION_CACHE_FILE, "w") as f:
             json.dump(data, f, indent=4, ensure_ascii=False)
@@ -117,8 +99,8 @@ class CustomNotifications(Notifications):
 
     def get_deserialized(self) -> List[Notification]:
         """Return the notifications."""
-        if len(self.deserialized_notifications) <= 0:
+        if not self.deserialized_notifications:
             self.deserialized_notifications = [
-                Notification.deserialize(data) for data in self._notifications
+                Notification.deserialize(data) for data in self.all_notifications
             ]
         return self.deserialized_notifications
