66f87ba7bcbda3ba6ed7c762fe6a7fc5353456e3
Rubin Bhandari <roobin.bhandari@gmail.com>
2025-06-08 23:43:20 +0545

    refactor: optimize workspace button setup and improve label creation logic

--- widgets/workspaces.py
+++ widgets/workspaces.py
@@ -1,5 +1,4 @@
-from fabric.hyprland.widgets import WorkspaceButton as WsButton
-from fabric.hyprland.widgets import Workspaces
+from fabric.hyprland.widgets import WorkspaceButton, Workspaces
 
 from shared.widget_container import BoxWidget
 from utils.functions import unique_list
@@ -11,31 +10,29 @@ class WorkSpacesWidget(BoxWidget):
     def __init__(self, **kwargs):
         super().__init__(name="workspaces", **kwargs)
 
-        # Convert ignored workspace IDs to integers
-        ignored_ws = [int(x) for x in unique_list(self.config["ignored"])]
-        default_format = self.config.get("default_label_format", "{id}")
+        config = self.config
+        ignored_ws = {int(x) for x in unique_list(config.get("ignored", []))}
+        icon_map = config.get("icon_map", {})
+        default_format = config.get("default_label_format", "{id}")
+        workspace_count = config.get("count", 8)
+        hide_unoccupied = config.get("hide_unoccupied", False)
 
         def create_workspace_label(ws_id: int) -> str:
-            # First check icon_map for custom label/icon
-            str_id = str(ws_id)
-            if str_id in self.config.get("icon_map", {}):
-                return self.config["icon_map"][str_id]
+            return icon_map.get(str(ws_id), default_format.format(id=ws_id))
 
-            # Otherwise use default format
-            return default_format.format(id=ws_id)
-
-        def setup_button_empty_state(button):
-            """Set up empty state tracking for workspace button"""
+        def setup_button(ws_id: int) -> WorkspaceButton:
+            button = WorkspaceButton(
+                id=ws_id,
+                label=create_workspace_label(ws_id),
+                visible=ws_id not in ignored_ws,
+            )
 
             def update_empty_state(*_):
-                if button.get_empty():
-                    button.add_style_class("unoccupied")
-                else:
-                    button.remove_style_class("unoccupied")
+                button.add_style_class(
+                    "unoccupied"
+                ) if button.empty else button.remove_style_class("unoccupied")
 
             button.connect("notify::empty", update_empty_state)
-
-            # Set initial state
             update_empty_state()
             return button
 
@@ -44,25 +41,17 @@ class WorkSpacesWidget(BoxWidget):
             name="workspaces",
             spacing=4,
             # Create buttons for each workspace if occupied
-            buttons=None  # sending None to buttons will create occupied workspaces only
-            if self.config["hide_unoccupied"]
+            buttons=None
+            if hide_unoccupied
             else [
-                setup_button_empty_state(
-                    WsButton(id=i, label=create_workspace_label(i))
-                )
-                for i in range(1, self.config["count"] + 1)
-                if i not in ignored_ws
+                setup_button(ws_id)
+                for ws_id in range(1, workspace_count + 1)
+                if ws_id not in ignored_ws
             ],
             # Factory function to create buttons for each workspace
-            buttons_factory=lambda ws_id: setup_button_empty_state(
-                WsButton(
-                    id=ws_id,
-                    label=create_workspace_label(ws_id),
-                    visible=ws_id not in ignored_ws,
-                )
-            ),
-            invert_scroll=self.config["reverse_scroll"],
-            empty_scroll=self.config["empty_scroll"],
+            buttons_factory=setup_button,
+            invert_scroll=config.get("reverse_scroll", False),
+            empty_scroll=config.get("empty_scroll", False),
         )
 
         # Add the HyperlandWorkspace widget as a child
