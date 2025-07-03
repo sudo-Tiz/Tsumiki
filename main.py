19cd8222f2a30effa125f12bf7cd2a16c837185c
Rubin Bhandari <roobin.bhandari@gmail.com>
2025-07-03 06:09:03 +0545

    docs: update README to include former name and add image asset

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+
 import setproctitle
 from fabric import Application
 from fabric.utils import exec_shell_command, get_relative_path
+from gi.repository import GLib
 from loguru import logger
 
 import utils.functions as helpers
@@ -19,11 +21,11 @@ def process_and_apply_css(app: Application):
 
     if output == "":
         logger.info(f"{Colors.INFO}[Main] CSS applied")
-        app.set_stylesheet_from_file(get_relative_path("dist/main.css"))
+        GLib.idle_add(app.set_stylesheet_from_file, get_relative_path("dist/main.css"))
     else:
         logger.exception(f"{Colors.ERROR}[Main]Failed to compile sass!")
         logger.exception(f"{Colors.ERROR}[Main] {output}")
-        app.set_stylesheet_from_string("")
+        GLib.idle_add(app.set_stylesheet_from_string, "")
 
 
 general_options = widget_config["general"]
@@ -97,6 +99,7 @@ def main():
 
     logger.info(f"{Colors.INFO}[Main] Starting {APPLICATION_NAME}...")
     logger.info(f"Starting shell... pid:{os.getpid()}")
+
 
 if __name__ == "__main__":
     main()
