466becc0835c0cdf32980694026f0019aa1ab8a0
Rubin Bhandari <roobin.bhandari@gmail.com>
2025-08-08 07:57:44 +0545

    refactor: update thumbnail generation to use PIL and run in thread pool

--- utils/thread.py
+++ utils/thread.py
@@ -2,7 +2,7 @@ from collections.abc import Callable
 from concurrent.futures import ThreadPoolExecutor
 
 # Create a shared thread pool
-_executor = ThreadPoolExecutor(max_workers=4)
+thread_pool = ThreadPoolExecutor(max_workers=4)
 
 
 def thread(target: Callable, *args, **kwargs):
@@ -10,7 +10,7 @@ def thread(target: Callable, *args, **kwargs):
     Submit the given function to the thread pool.
     Returns a Future instead of a Thread.
     """
-    return _executor.submit(target, *args, **kwargs)
+    return thread_pool.submit(target, *args, **kwargs)
 
 
 def run_in_thread(func: Callable) -> Callable:
