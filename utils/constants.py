e5f1de49d1b573f803ec0e590b6a8871b84d3686
Rubin Bhandari <roobin.bhandari@gmail.com>
2025-08-20 10:15:13 +0545

    fix: adjust truncation size and disable mappings in window title configuration

--- utils/constants.py
+++ utils/constants.py
@@ -315,7 +315,7 @@ DEFAULT_CONFIG = {
         "window_title": {
             "icon": True,
             "truncation": True,
-            "truncation_size": 30,
+            "truncation_size": 20,
             "tooltip": True,
             "mappings": True,
             "title_map": [],
